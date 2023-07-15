9c9e8071403f58360bf61b8f45ee56637986f4f8
fengyiwu98 <wufengyi98@163.com>
2023-07-15 16:16:33 +0800

    modified input format of datasets

--- utils/images.py
+++ utils/images.py
@@ -7,7 +7,8 @@ def load_image(srcpath):
     img=cv2.imdecode(np.fromfile(srcpath, dtype=np.uint8), -1)
     if img.ndim == 3:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = img / 255
+    # img = img / 255
+    # img = cv2.imread(srcpath, 0)
     return img
 
 
